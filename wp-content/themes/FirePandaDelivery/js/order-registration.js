48c80b7c70a521a801a67e0bc11208967b30e7a8
Irina Khmyrova <Ira.khmyrova@yandex.ru>
2025-04-27 23:19:57 +0300

    перенос в old

--- wp-content/themes/FirePandaDelivery/js/order-registration.js
+++
@@ -1,9 +0,0 @@
-let radioOrderBtn = document.querySelector("#radioOrderBtn");
-let radioOrderBtn2 = document.querySelector("#radioOrderBtn2");
-
-radioOrderBtn.addEventListener( 'click', () =>{
-    radioOrderBtn2.value = 'false';
-});
-radioOrderBtn2.addEventListener( 'click', () =>{
-    radioOrderBtn.value = 'false';
-});
